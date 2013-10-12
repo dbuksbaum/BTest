bc6ea16cf120983010ca918f86fe7614ffa3076e
David Buksbaum <david.buksbaum@gmail.com>
2013-10-11 22:34:27 -0400

    Some work to add ember

--- js/models/todo.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by david on 10/11/13.
- */
-
-Todos.Todo = DS.Model.extend({
-    title: DS.attr('string'),
-    isCompleted: DS.attr('boolean')
-});
--- js/router.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by david on 10/11/13.
- */
-
-Todos.Router.map(function() {
-    "use strict";
-    this.resource('todos', { path: '/' });
-});
-
